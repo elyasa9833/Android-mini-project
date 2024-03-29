4c6fa6af7c1f21ae8e172c24b41a72682bf90327
Amri Elyasa <aelyasa09@gmail.com>
2024-03-30 00:30:02 +0700

    feat: input field - warning while data empty

--- app/src/main/java/org/d3if0043/monefysafe/ui/screen/SecondScreen.kt
+++ app/src/main/java/org/d3if0043/monefysafe/ui/screen/SecondScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -26,8 +27,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -83,9 +85,11 @@ fun InputContent(modifier: Modifier){
         stringResource(id = R.string.withdraw)
     )
 
-    var type by remember { mutableStateOf(radioOption[0]) }
-    var jumlahUang by remember { mutableStateOf("") }
-    var keterangan by remember { mutableStateOf("") }
+    var type by rememberSaveable { mutableStateOf(radioOption[0]) }
+    var jumlahUang by rememberSaveable { mutableStateOf("") }
+    var hasil by rememberSaveable { mutableIntStateOf(0) }
+    var jumlahError by rememberSaveable { mutableStateOf(false) }
+    var keterangan by rememberSaveable { mutableStateOf("") }
 
     Column(
         modifier = modifier.padding(16.dp)
@@ -93,7 +97,11 @@ fun InputContent(modifier: Modifier){
         OutlinedTextField(
             value = jumlahUang,
             onValueChange = {jumlahUang = it},
-            label = { Text("Jumlah Uang") },
+            label = { Text(stringResource(id = R.string.jumlah)) },
+            isError = jumlahError,
+            leadingIcon = { Text(text = stringResource(id = R.string.leadIcon)) },
+            trailingIcon = { IconPicker(isError = jumlahError) },
+            supportingText = { ErrorHint(isError = jumlahError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -104,8 +112,9 @@ fun InputContent(modifier: Modifier){
         OutlinedTextField(
             value = keterangan,
             onValueChange = {keterangan = it},
-            label = { Text("Keterangan") },
+            label = { Text(stringResource(id = R.string.keterangan)) },
             minLines = 3,
+            maxLines = 3,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text,
                 imeAction = ImeAction.Done
@@ -132,9 +141,15 @@ fun InputContent(modifier: Modifier){
                 )
             }
         }
+        Text(text = hasil.toString())
         Spacer(modifier = Modifier.weight(1f))
         Button(
-            onClick = { /* TODO */ },
+            onClick = {
+                jumlahError = (jumlahUang == "" || jumlahUang == "0")
+                if(jumlahError) return@Button
+
+                hasil = jumlahUang.toInt()
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp),
@@ -142,6 +157,7 @@ fun InputContent(modifier: Modifier){
         ) {
             Text(text = "Simpan")
         }
+
     }
 }
 
@@ -161,6 +177,16 @@ fun transactionTypes(label: String, isSelected: Boolean, modifier: Modifier){
             modifier = Modifier.padding(start = 8.dp)
         )
     }
+}
+
+@Composable
+fun IconPicker(isError: Boolean){
+    if(isError) Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+}
+
+@Composable
+fun ErrorHint(isError: Boolean){
+    if(isError) Text(text = stringResource(id = R.string.invalid_input))
 }
 
 @Preview(showBackground = true)
