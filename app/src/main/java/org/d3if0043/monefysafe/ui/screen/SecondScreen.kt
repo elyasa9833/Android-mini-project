2c5e304df8536259cba220c2df8b0c9aec339425
Amri Elyasa <aelyasa09@gmail.com>
2024-03-29 16:23:11 +0700

    feat: SecondScreen - create TextField, RadioButton

--- app/src/main/java/org/d3if0043/monefysafe/ui/screen/SecondScreen.kt
+++ app/src/main/java/org/d3if0043/monefysafe/ui/screen/SecondScreen.kt
@@ -1,20 +1,37 @@
 package org.d3if0043.monefysafe.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if0043.monefysafe.R
@@ -43,15 +60,75 @@ fun SecondScreen(){
 
 @Composable
 fun InputContent(modifier: Modifier){
+    val radioOption = listOf(
+        "Deposit",
+        "Withdraw"
+    )
+
+    var type by remember { mutableStateOf(radioOption[0]) }
+    var jumlahUang by remember { mutableStateOf("") }
+    var keterangan by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        Text(text = "Hello World")
+        OutlinedTextField(
+            value = jumlahUang,
+            onValueChange = {jumlahUang = it},
+            label = { Text("Jumlah Uang") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = keterangan,
+            onValueChange = {keterangan = it},
+            label = { Text("Keterangan") },
+            minLines = 3,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ){
+            radioOption.forEach { text ->
+                transactionTypes(
+                    label = text,
+                    isSelected = type == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = type == text,
+                            onClick = { type = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = { /* TODO */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
 
 @Composable
-fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier){
+fun transactionTypes(label: String, isSelected: Boolean, modifier: Modifier){
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
