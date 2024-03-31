bced8b899939084d7b0cc1a4e8bae44f2525ed8a
Amri Elyasa <aelyasa09@gmail.com>
2024-04-01 05:57:35 +0700

    refactor: rename MainScreen and SecondScreen

--- app/src/main/java/org/d3if0043/monefysafe/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0043/monefysafe/ui/screen/MainScreen.kt
@@ -1,35 +1,48 @@
 package org.d3if0043.monefysafe.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.d3if0043.monefysafe.R
-import org.d3if0043.monefysafe.model.Transaksi
 import org.d3if0043.monefysafe.navigation.Screen
 import org.d3if0043.monefysafe.ui.theme.MonefySafeTheme
 
@@ -60,60 +73,126 @@ fun MainScreen(navController: NavHostController){
             }
         }
     ) {padding ->
-        ScreenContent(Modifier.padding(padding))
+        InputContent(Modifier.padding(padding))
 
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier){
-    val viewModel: MainViewModel = viewModel()
-    val data =  viewModel.data
+fun InputContent(modifier: Modifier){
+    val radioOption = listOf(
+        stringResource(id = R.string.deposit),
+        stringResource(id = R.string.withdraw)
+    )
 
-    if(data.isEmpty()){
-        Column (
-            modifier = modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+    var type by rememberSaveable { mutableStateOf(radioOption[0]) }
+    var jumlahUang by rememberSaveable { mutableStateOf("") }
+    var hasil by rememberSaveable { mutableIntStateOf(0) }
+    var jumlahError by rememberSaveable { mutableStateOf(false) }
+    var keterangan by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = jumlahUang,
+            onValueChange = {jumlahUang = it},
+            label = { Text(stringResource(id = R.string.jumlah)) },
+            isError = jumlahError,
+            leadingIcon = { Text(text = stringResource(id = R.string.leadIcon)) },
+            trailingIcon = { IconPicker(isError = jumlahError) },
+            supportingText = { ErrorHint(isError = jumlahError) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = keterangan,
+            onValueChange = {keterangan = it},
+            label = { Text(stringResource(id = R.string.keterangan)) },
+            minLines = 3,
+            maxLines = 3,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
         ){
-            Text(text = stringResource(id = R.string.data_kosong))
-        }
-    }else{
-        LazyColumn(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(bottom = 84.dp)
-        ){
-            items(data){
-                ListItem(transaksi = it)
-                Divider()
+            radioOption.forEach { text ->
+                transactionTypes(
+                    label = text,
+                    isSelected = type == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = type == text,
+                            onClick = { type = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
             }
         }
+        Text(text = hasil.toString())
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {
+                jumlahError = (jumlahUang == "" || jumlahUang == "0")
+                if(jumlahError) return@Button
+
+                hasil = jumlahUang.toInt()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = "Simpan")
+        }
 
     }
 }
 
 @Composable
-fun ListItem(transaksi: Transaksi){
-    Column (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ){
-        Text(text = transaksi.Jumlah.toString())
-        Text(text = transaksi.Jenis)
-        Text(text = transaksi.keterangan)
-        Text(text = transaksi.tanggal)
+fun transactionTypes(label: String, isSelected: Boolean, modifier: Modifier){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = isSelected,
+            onClick = null
+        )
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
+}
+
+@Composable
+fun IconPicker(isError: Boolean){
+    if(isError) Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+}
+
+@Composable
+fun ErrorHint(isError: Boolean){
+    if(isError) Text(text = stringResource(id = R.string.invalid_input))
 }
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SecondPreview() {
     MonefySafeTheme {
         MainScreen(rememberNavController())
     }
